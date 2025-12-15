09fa4aaab513da112cb29db3f8e8f10e674c39e7
Engrasia Masyithah <2522500074@mahasiswa.atmaluhur.ac.id>
2025-12-15 22:33:45 +0700

    menghapus

--- pertemuan-11/read.php
+++ pertemuan-11/read.php
@@ -21,15 +21,4 @@ $q = mysqli_query($conn, $sql);
 <?php endwhile; ?>
 </table>
 
-<!-- Di dalam file read.php, temukan bagian header tabel (<thead>) -->
-<thead>
-    <tr>
-        <th>No</th> <!-- Tambahkan baris ini -->
-        <th>ID</th>
-        <th>Nama</th>
-        <th>Email</th>
-        <th>Pesan</th>
-        <th>Created At</th> <!-- Untuk tugas no 5 -->
-    </tr>
-</thead>
 
