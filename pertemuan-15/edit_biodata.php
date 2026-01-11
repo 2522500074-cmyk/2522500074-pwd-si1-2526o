113273aad8554230c683215219a1fc5616fcb31d
Engrasia Masyithah <2522500074@mahasiswa.atmaluhur.ac.id>
2026-01-11 22:52:35 +0700

    mengedit kode file editbiodataphp

--- pertemuan-15/edit_biodata.php
+++ pertemuan-15/edit_biodata.php
@@ -2,42 +2,17 @@
   session_start();
   require 'koneksi.php';
   require 'fungsi.php';
-
-  /*
-    Ambil nilai cid dari GET dan lakukan validasi untuk 
-    mengecek cid harus angka dan lebih besar dari 0 (> 0).
-    'options' => ['min_range' => 1] artinya cid harus ≥ 1 
-    (bukan 0, bahkan bukan negatif, bukan huruf, bukan HTML).
-  */
+  
   $cid = filter_input(INPUT_GET, 'cid', FILTER_VALIDATE_INT, [
     'options' => ['min_range' => 1]
   ]);
-  /*
-    Skrip di atas cara penulisan lamanya adalah:
-    $cid = $_GET['cid'] ?? '';
-    $cid = (int)$cid;
 
-    Cara lama seperti di atas akan mengambil data mentah 
-    kemudian validasi dilakukan secara terpisah, sehingga 
-    rawan lupa validasi. Untuk input dari GET atau POST, 
-    filter_input() lebih disarankan daripada $_GET atau $_POST.
-  */
 
-  /*
-    Cek apakah $cid bernilai valid:
-    Kalau $cid tidak valid, maka jangan lanjutkan proses, 
-    kembalikan pengguna ke halaman awal (read.php) sembari 
-    mengirim penanda error.
-  */
   if (!$cid) {
     $_SESSION['flash_error'] = 'Akses tidak valid.';
     redirect_ke('read_biodata.php');
   }
-
-  /*
-    Ambil data lama dari DB menggunakan prepared statement, 
-    jika ada kesalahan, tampilkan penanda error.
-  */
+  
   $stmt = mysqli_prepare($conn, "SELECT cid, cnim,	cnama_lengkap,	ctempat_lahir,	ctanggal_lahir,	chobi,	cpasangan,
 	cpekerjaan,	cnamaorangtua,	cnama_kakak,	cnama_adik
                                     FROM tbl_biodata_mahasiswa_sederhana WHERE cid = ? LIMIT 1");
@@ -57,7 +32,7 @@
     redirect_ke('read_biodata).php');
   }
 
-  #Nilai awal (prefill form)
+
   $nim  = $row['cnim'] ?? '';
   $nama = $row['cnama_lengkap'] ?? '';
   $tempat_lahir = $row['ctempat_lahir'] ?? '';
@@ -68,7 +43,7 @@
     $nama_ortu = $row['cnamaortu'] ?? '';
     $nama_kakak = $row['cnama_kakak'] ?? '';
     $nama_adik = $row['cnama_adik'] ?? '';
-  #Ambil error dan nilai old input kalau ada
+ 
  $flash_error = $_SESSION['flash_error'] ?? '';
   $old_biodata = $_SESSION['old_biodata'] ?? [];
   unset($_SESSION['flash_error'], $_SESSION['old_biodata']);
